de7a53801506803e042d8a4b5bb12cac6d72e825
Kha Pham <Khathepham@gmail.com>
2024-05-17 14:36:10 -0500

    project setup

---
+++ backend/src/main/kotlin/com/example/backend/blog/BlogApplication.kt
@@ -0,0 +1,4 @@
+package com.example.backend.blog
+
+class BlogApplication {
+}
---
+++ backend/src/main/kotlin/com/example/backend/dso/User.kt
@@ -0,0 +1,6 @@
+package com.example.backend.dso
+
+data class User(val fname: String, val lname:String,
+                var username:String, var password:String ) {
+
+}
